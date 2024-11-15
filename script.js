ecd54d6c4cd1e443d02c7e3ea33529bf6a42d3d3
chovy <feichovy202221190050@hotmail.com>
2024-11-15 16:24:35 +0000

    modification 15/11/2024

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-function addTask(){
-    const taskInput=document.getElementById('taskInput');
-    const taskText = taskInput.value.trim();
-
-    if(taskText !== ""){
-        const taskList = document.getElementById('taskList');
-
-        // Create list attribute
-        const listItem = document.createElement('li');
-        listItem.textContent = taskText;
+// 用addEventListener，设置<button>ID, 避免在HTML中用Onclck属性
+document.getElementById('addTaskButton').addEventListener('click', addTask);
+// 用回车键增加任务
+document.getElementById('taskInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+// 创建任务的html属性，模块化开发
+function createTaskElement(taskText) {
+    const listItem = document.createElement('li');
+    listItem.textContent = taskText;
 
     const completeButton = document.createElement('button');
-    completeButton.textContent="Complete";
-    completeButton.onclick=function(){
+    completeButton.textContent = "Complete";
+    completeButton.onclick = function () {
         listItem.classList.toggle('complete');
     };
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = "Delete";
-    deleteButton.className="delete-button";
-    deleteButton.onclick=function(){
-        taskList.removeChild(listItem);
+    deleteButton.className = "delete-button";
+    deleteButton.onclick = function () {
+        listItem.parentElement.removeChild(listItem);
     };
 
     listItem.appendChild(completeButton);
     listItem.appendChild(deleteButton);
 
-    taskList.appendChild(listItem);
+    return listItem;
+}
+// 将新创建的任务添加到列表
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
 
-    taskInput.value="";
+    if (taskText === "") {
+        // alert("Task cannot be empty. Please enter a task.")
+        errormessage.textContent = "Task cannot be empty. Please enter a task.";
+        return;
     }
+
+    errormessage.textContent="";
+    const taskList = document.getElementById('taskList');
+    const newTask = createTaskElement(taskText);
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";// 清除输入框内容
 }
