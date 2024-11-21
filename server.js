076cde7e563cde432f100905517a3183d4e4b88d
chovy <feichovy202221190050@hotmail.com>
2024-11-21 21:54:57 +0000

    cao

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser'); // 导入中间件 body-parser
 const path = require('path'); // 引入 Path 路径处理模块
 const userRoutes = require('./routes/users'); // 导入用户路由
 const taskRoutes = require('./routes/tasks'); // 导入任务路由
-
+const cors = require('cors') // 添加CORS支持
 const PORT = 3000;
 const app = express(); // 创建 Express 实例
-
+app.use(express.json());
 // 静态文件服务
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 添加后端中间件
+const cors = require('cors');
 // 使用中间件解析 JSON 和 URL 编码数据
 app.use(bodyParser.json()); // 解析 JSON 格式的数据
 app.use(bodyParser.urlencoded({ extended: true })); // 解析 URL 编码的数据
@@ -20,5 +21,5 @@ app.use('/tasks', taskRoutes);
 
 // 启动服务器
 app.listen(PORT, () => {
-    console.log(`Server is running at http://47.242.219.237:${PORT}`);
+    console.log(`Server running at http://47.242.219.237:${PORT}`);
 });
